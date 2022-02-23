a3ec4173f8f7644d448819d128d41e3d54619fc9
York <mengerpapa@gmail.com>
2022-02-22 21:48:10 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB =
+var dev_db_url =
   "mongodb+srv://glasboy:glassboylluuyy@cluster0.v6ocq.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
